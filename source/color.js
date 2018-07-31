2c00d306ed5055737e066f943f57d103ef4974f7
Misha <misha@myrt.co>
2018-08-01 00:42:54 +0300

    Formatting, js-beautify in package.json🔥

--- source/color.js
+++ source/color.js
@@ -1,12 +1,10 @@
 export const hexToRGB = (hex) => {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ?
-        {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16),
-        } :
-        null
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+    } : null
 }
 
 export const contrastLevel = (rgb) => Math.round(
